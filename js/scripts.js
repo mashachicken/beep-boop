eb30d37016ac6dc9079d4c75abca9ce33caa829f
Masha Ivy <mashanechmo@gmail.com>
2019-11-01 10:05:49 -0700

    add few functions to scripts

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+(document).ready(function(){    //bussiness
+    $("form#enterNumber").submit(function(event) {
+
+
+or (var i=0; i<=numberInput; i++) {
+     if (i.toString().includes("3")) {
+        newArray.push("I'm sorry, Dave. I'm afraid I can't do that.");
+        } else if (i.toString().includes("2")) {
+            newArray.push("boop!")
+        } else if (i.toString().includes("1")) {
+            newArray.push("beep!")
