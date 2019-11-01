768638a7b442e85652cb625a486c29ae593c948f
Masha Ivy <mashanechmo@gmail.com>
2019-11-01 15:19:51 -0700

    rewrite js with a different logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,32 @@
-(document).ready(function(){ //user
-    $("form#enterNumber").submit(function(event) {
-      event.preventDefault();
-  $(".bubble").hide()
+function beepBoop(numberInput){
+var beep = "1";
+var boop = "2";
+var sorry = "3";
+
+
+
+  var result= [];
+
+  for (var i=numberInput; i>=0; --i) {
+    result.push(console.log(i))
+    console.log(result)
+    if (result.toString().includes("3")) {
+      console.log(i)
+      result.push("I'm sorry, Dave. I'm afraid I can't do that.");
+    } else if (i.toString().includes("2")) {
+      result.push("boop!")
+    } else if (i.toString().includes("1")) {
+      result.push("beep!")
+    };
+  };
+  return result;
+};
+$(document).ready(function(){ //user
+  $("form#enterNumber").submit(function(event) {
+  event.preventDefault();
   var numberInput = parseInt($("input").val());
   var result = beepBoop(numberInput)
   $("#responces1").append(result)
-  console.log(newArray)
 
 });
 });
-
-//bussiness
-  function beepBoop(numberInput) {
-    for (var i=0; i<=numberInput; i++) {
-     if (i.toString().includes("3")) {
-        newArray.push("I'm sorry, Dave. I'm afraid I can't do that.");
-        } else if (i.toString().includes("2")) {
-            newArray.push("boop!")
-        } else if (i.toString().includes("1")) {
-            newArray.push("beep!")
-          };
-return newArray;
-};
-var newArray = [];
