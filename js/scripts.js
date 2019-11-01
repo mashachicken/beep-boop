1c3ab42e17632202ad124a09ec146f49755075bc
Masha Ivy <mashanechmo@gmail.com>
2019-11-01 10:53:08 -0700

    add few functions to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
-(document).ready(function(){    //bussiness
+(document).ready(function(){
     $("form#enterNumber").submit(function(event) {
+      event.preventDefault();
+  $(".bubble").hide()
+  var numberInput = parseInt($("input").val());
+  var result = beepBoop(numberInput)
+  $("#responces1").append(result)
+  console.log(newArray)
 
+});
+});
 
-or (var i=0; i<=numberInput; i++) {
+//bussiness
+function beepBoop(numberInput) {
+    for (var i=0; i<=numberInput; i++) {
      if (i.toString().includes("3")) {
         newArray.push("I'm sorry, Dave. I'm afraid I can't do that.");
         } else if (i.toString().includes("2")) {
