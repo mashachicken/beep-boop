b9508a4291c960fd6601ac81be8d933fa86df3de
Masha Ivy <mashanechmo@gmail.com>
2019-11-01 17:10:05 -0700

    remove a global variable and make some changes to css to make it a little less ugly

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,24 @@
 $(document).ready(function(){
-    $("form#enterNumber").submit(function(event) {
-        event.preventDefault();
-        var numberInput = parseInt($("input").val());
-        var result = beepBoop(numberInput)
-        $("#responces1").append(result)
-        console.log(newArray)
-
+  $("form#enterNumber").submit(function(event) {
+    event.preventDefault();
+    var numberInput = parseInt($("input").val());
+    var result = beepBoop(numberInput)
+    $("#responces1").append(result)
+    console.log(newArray)
     });
 });
+var newArray = [];
 function beepBoop(numberInput) {
-    for (var i=0; i<=numberInput; i++) {
-         if (i.toString().includes("3")) {
-            newArray.push("I'm sorry, Dave. I'm afraid I can't do that.");
-            } else if (i.toString().includes("2")) {
-                newArray.push("boop!")
-            } else if (i.toString().includes("1")) {
-                newArray.push("beep!")
-            } else {
-                newArray.push(i);
-            }
-    };
-    return newArray;
-    };
-    var newArray = [];
+  for (var i=0; i<=numberInput; i++) {
+    if (i.toString().includes("3")) {
+      newArray.push("I'm sorry, Dave. I'm afraid I can't do that.");
+    } else if (i.toString().includes("2")) {
+      newArray.push("boop!")
+    } else if (i.toString().includes("1")) {
+      newArray.push("beep!")
+    } else {
+      newArray.push(i);
+}
+};
+  return newArray;
+};
